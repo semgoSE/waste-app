cd284b4e7b4c87fca61d15ec2e3d0f8d691dcab9
semgoSE <sem1234d@gmail.com>
2020-04-24 23:26:13 +0300

    Updates

---
+++ precache-manifest.9be91756ab85d129647cb405041e2f12.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54de374d0a98fe7085799ce7acc9df7b",
+    "url": "/waste-app/index.html"
+  },
+  {
+    "revision": "35e9cb5a1191bcdd96db",
+    "url": "/waste-app/static/css/2.abebce0a.chunk.css"
+  },
+  {
+    "revision": "35e9cb5a1191bcdd96db",
+    "url": "/waste-app/static/js/2.38fbdb6e.chunk.js"
+  },
+  {
+    "revision": "9545d2ff040f1d230f20c359158fcfad",
+    "url": "/waste-app/static/js/2.38fbdb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be25fdb3cf5c035eb0f",
+    "url": "/waste-app/static/js/main.37739a7e.chunk.js"
+  },
+  {
+    "revision": "23e2d1634356dea3df4f",
+    "url": "/waste-app/static/js/runtime-main.2a964f94.js"
+  },
+  {
+    "revision": "303e24db580cd7541028b7b9768a76e5",
+    "url": "/waste-app/static/media/bin.303e24db.png"
+  },
+  {
+    "revision": "3168a39b79bae2205ea7cb32cbaf6f09",
+    "url": "/waste-app/static/media/glass.3168a39b.png"
+  },
+  {
+    "revision": "86c04338a35ed8724a0dbac14e95923e",
+    "url": "/waste-app/static/media/metal.86c04338.png"
+  },
+  {
+    "revision": "eba8e967236582c27142cb6140a74013",
+    "url": "/waste-app/static/media/other.eba8e967.png"
+  },
+  {
+    "revision": "532879c5b5f7800cf6014e9108057835",
+    "url": "/waste-app/static/media/paper.532879c5.png"
+  },
+  {
+    "revision": "de4a7505c3de4461d7548b74b36603c1",
+    "url": "/waste-app/static/media/plastic.de4a7505.png"
+  }
+]);
